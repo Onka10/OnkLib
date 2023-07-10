7c961c535efec71285ed2f2e6a32c73250182f08
onkalo <87790609+Onka10@users.noreply.github.com>
2023-07-10 20:02:18 +0900

    u1w6

---
+++ Utility/Static/InstantDebug.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public static class InstantDebug
+{
+    // public static void DebugLog(){
+    //     Debug.Log("テスト");
+    // }
+
+    // public static void DebugLog(int n){
+    //     Debug.Log("テスト"+n);
+    // }
+}
